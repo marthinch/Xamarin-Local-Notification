f47e9f4b8304fc623e7f56167c45bbcb08dcec1e
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-08-17 14:14:05 +0800

    INITIALIZE PROJECT & GIT IGNORE

---
+++ LocalNotification/LocalNotification/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalNotification
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonTestLocalNotification_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Test local notification", "This is local notification");
+        }
+
+        private void ButtonTestLocalNotificationByTime_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Test local notification", "This is local notification by time", 1, DateTime.Now.AddSeconds(10));
+        }
+    }
+}
